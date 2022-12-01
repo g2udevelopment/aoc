5b27605c9882ba3f2ea24fb80c8cf820692cf89a
Gerben van de Wiel <g.van.de.wiel@betabit.nl>
2022-12-01 15:49:53 +0100

    Day1

---
+++ day1/Input.cs
@@ -0,0 +1,28 @@
+/// Wraps the input
+public class Input
+{
+    string[] lines;
+    public Input(string filename) {
+        lines = File.ReadAllLines("input.txt");
+    }
+
+    public List<List<string>> batchBy(string sep) {
+{
+    var localList = new List<string>();
+    var globalList = new List<List<string>>();
+    foreach (var line in lines)
+    {
+        if (line == sep)
+        {
+            globalList.Add(localList);
+            localList = new List<string>();
+        }
+        else
+        {
+            localList.Add(line);
+        }
+    }
+    return globalList;
+};
+}
+}
---
+++ day1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+var input = new Input("input.txt");
+var calsPerDwarf = input.batchBy("");
+
+// Solution1
+var highest = CalcPerDwarf(calsPerDwarf).Take(1).Single();
+System.Console.WriteLine($"Solution to problem1: {highest} ");
+
+// Solution 2
+var highest3 = CalcPerDwarf(calsPerDwarf).Take(3).Sum();
+System.Console.WriteLine($"Solution problem2: {highest3}");
+
+static IOrderedEnumerable<int> CalcPerDwarf(List<List<string>> calsPerDwarf)
+{
+    return calsPerDwarf.Select(dwarf => dwarf.Select(c => int.Parse(c)).Sum()).OrderDescending();
+}
