a5fa8d51da6d54024f4417096f2fe77e69a808d7
Gerben van de Wiel <g.van.de.wiel@betabit.nl>
2022-12-06 18:05:48 +0100

    Day6

---
+++ day6/Program.cs
@@ -0,0 +1,18 @@
+﻿var line = File.ReadLines("input.txt").Single();
+
+System.Console.WriteLine($"Solution1: {FindMarker(line,4)}");
+System.Console.WriteLine($"Solution1: {FindMarker(line,14)}");
+
+
+static int FindMarker(string line, int length)
+{
+    for (int i = 0; i < line.Length - length-1; i++)
+    {
+        var marker = line.Substring(i, length);
+        if (marker.Distinct().Count() == length) // Marker found
+        { 
+            return i + length;
+        }
+    }
+    throw new ArgumentException("Marker not found");
+}
