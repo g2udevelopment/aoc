a770aa56e1411106706d12d9f42bb6e9c7059702
Gerben van de Wiel <g.van.de.wiel@betabit.nl>
2022-12-10 21:24:19 +0100

    Some refactoring

--- day9/Program.cs
+++ day9/Program.cs
@@ -1,12 +1,69 @@
 ﻿var lines = File.ReadAllLines("input.txt");
 
-HashSet<(int, int)> visited = new HashSet<(int, int)>();
-visited.Add((0, 0)); // start with initial pos visitied
+Solution(lines,2);
+Solution(lines,10);
 
-//var knot1 = (0,0);
-var knot2 = (0, 0);
+bool toFar((int x, int y) delta)
+{
+    return Math.Abs(delta.x) > 1 || Math.Abs(delta.y) > 1;
+}
 
+static (int, int) ParseMove(char direction)
+{
+    return direction switch
+    {
+        'R' => (1, 0),
+        'L' => (-1, 0),
+        'U' => (0, 1),
+        'D' => (0, -1),
+        _ => throw new ArgumentException()
+    };
+}
 
+static (int, int) Move((int x, int y) point, (int x, int y) direction)
+{
+    return (point.x + direction.x, point.y + direction.y);
+}
+
+static (int, int) Delta((int x, int y) head, (int x, int y) tail)
+{
+    return (head.x - tail.x, head.y - tail.y);
+}
+
+static (int, int) Clamp((int x, int y) point)
+{
+    return (Math.Clamp(point.x, -1, 1), Math.Clamp(point.y, -1, 1));
+}
+
+List<(int, int)> ParseRope(string[] lines, HashSet<(int, int)> visited, List<(int, int)> rope)
+{
+    foreach (var line in lines)
+    {
+        var times = int.Parse(line[2..]);
+        for (int i = 0; i < times; i++)
+        {
+            var dir = ParseMove(line[0]);
+            var index = 0;
+            var newrope = new List<(int, int)>();
+            foreach (var knot in rope) // Move a knots
+            {
+                index++;//1
+                var newknot = Move(knot, dir);//move dir
+                newrope.Add(newknot); //1 in newrop
+                if (index < rope.Count())
+                {
+                    var knotbehind = rope.Skip(index).First();
+                    var delta = Delta(newknot, knotbehind);
+                    dir = toFar(delta) ? Clamp(delta) : (0, 0);
+                }
+            }
+            rope = newrope;
+            visited.Add(rope.Last());
+        }
+    }
+
+    return rope;
+}
 
 static List<(int, int)> createRope(int knots)
 {
@@ -17,78 +74,11 @@ static List<(int, int)> createRope(int knots)
     }
     return rope;
 }
-var rop1 = createRope(10);
 
-foreach (var line in lines)
+void Solution(string[] lines, int knots)
 {
-    var split = line.Split(" ");
-    
-    var times = int.Parse(split[1]);
-    for (int i = 0; i < times; i++)
-    {
-        //var head = rop1.Take(1).Single();
-        //head = Move(head, dir);
-        // var tail = rop1.Skip(1);
-        var dir = ParseMove(split[0]);
-        var index = 0;
-        var newrope = new List<(int,int)>();
-        foreach (var head in rop1) // Move a knots
-        {
-            index++;//1
-            //Move(head, dir);
-            var newhead = Move(head, dir);//move dir
-            newrope.Add(newhead); //1 in newrop
-            System.Console.WriteLine(newhead);
-            if (index < rop1.Count())
-            {
-                System.Console.WriteLine($"index: {index}");
-                var tail = rop1.Skip(index).First();
-                var delta = Delta(newhead, tail);
-                if (toFar(delta))
-                {
-                    dir = Clamp(delta);
-                    //knot2 = Move(knot2,tailmove);
-                } else {
-                    dir = (0,0);
-                }
-            }
-        }
-        rop1 = newrope;
-        visited.Add(rop1.Last());
-        System.Console.WriteLine($"Tail now at: {rop1.Last()}");
-    }
-}
-
-System.Console.WriteLine($"Tail visisted: {visited.Count()}");
-
-bool toFar((int, int) delta)
-{
-    return Math.Abs(delta.Item1) > 1 || Math.Abs(delta.Item2) > 1;
-}
-
-static (int, int) ParseMove(string direction)
-{
-    return direction switch
-    {
-        "R" => (1, 0),
-        "L" => (-1, 0),
-        "U" => (0, 1),
-        "D" => (0, -1),
-        _ => throw new ArgumentException()
-    };
-}
-
-static (int, int) Move((int, int) point, (int, int) direction)
-{
-    return (point.Item1 + direction.Item1, point.Item2 + direction.Item2);
-}
-
-static (int, int) Delta((int, int) head, (int, int) tail)
-{
-    return (head.Item1 - tail.Item1, head.Item2 - tail.Item2);
-}
-
-static (int, int) Clamp((int, int) point)
-{
-    return (Math.Clamp(point.Item1, -1, 1), Math.Clamp(point.Item2, -1, 1));
+    HashSet<(int, int)> visited = new HashSet<(int, int)>();
+    visited.Add((0, 0)); // start with initial pos visitied
+    ParseRope(lines, visited, createRope(knots));
+    System.Console.WriteLine($"Sol1 visisted: {visited.Count()}");
 }
